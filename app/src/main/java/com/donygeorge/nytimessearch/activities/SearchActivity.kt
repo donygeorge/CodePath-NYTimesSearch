9b92f9ec5c3fe01c9345bf178f0b7d51c0a0cdf1
Dony George <donygeorge@gmail.com>
2017-09-24 12:42:17 -0700

    Update README

--- app/src/main/java/com/donygeorge/nytimessearch/activities/SearchActivity.kt
+++ app/src/main/java/com/donygeorge/nytimessearch/activities/SearchActivity.kt
@@ -44,6 +44,7 @@ class SearchActivity : AppCompatActivity() {
         val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
         rvResults.layoutManager = layoutManager
         mScrollListener = object : EndlessRecyclerViewScrollListener(layoutManager) {
+
             override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
                 // Triggered only when new data needs to be appended to the list
                 // Add whatever code is needed to append new items to the bottom of the list
