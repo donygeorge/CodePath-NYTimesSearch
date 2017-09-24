0d81dfc7231e8474ba1b1ec029e63fbba0886629
Dony George <donygeorge@gmail.com>
2017-09-24 11:52:22 -0700

    Add support for sharing

--- app/src/main/java/com/donygeorge/nytimessearch/activities/WebViewActivity.kt
+++ app/src/main/java/com/donygeorge/nytimessearch/activities/WebViewActivity.kt
@@ -1,16 +1,19 @@
 package com.donygeorge.nytimessearch.activities
 
 import android.annotation.TargetApi
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.ShareActionProvider
+import android.view.Menu
 import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.donygeorge.nytimessearch.R
 import kotlinx.android.synthetic.main.activity_web_view.*
-
 
 
 class WebViewActivity : AppCompatActivity() {
@@ -25,6 +28,20 @@ class WebViewActivity : AppCompatActivity() {
         myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         myWebView.webViewClient = MyBrowser()
         myWebView.loadUrl(url)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater = menuInflater
+        inflater.inflate(R.menu.webview_menu, menu);
+
+        val shareItem = menu!!.findItem(R.id.action_share)
+        val shareProvider = MenuItemCompat.getActionProvider(shareItem) as ShareActionProvider
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+        shareIntent.putExtra(Intent.EXTRA_TEXT, myWebView.getUrl())
+        shareProvider.setShareIntent(shareIntent)
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     private class MyBrowser : WebViewClient() {
